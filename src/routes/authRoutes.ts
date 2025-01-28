3520875aa6ad41b0ae1cce96c0a20f03dd2d34b2
danny <danny.mizrachi11@gmail.com>
2025-01-28 23:14:01 +0200

    code cleanup

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,26 +1,21 @@
 import {Router, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import {login, register} from "../services/user";
-import { sendEmailOtp } from "../services/mailer";
-import {createUrl} from "../services/url";
-
-
+import {sendEmailOtp} from "../services/mailer";
 dotenv.config();
-
 const router = Router();
 
 router.post('/register', async (req: Request, res: Response): Promise<void> => {
     const {email} = req.body;
     try {
         const user = await register(req);
-         await sendEmailOtp(email, user.id)
+        await sendEmailOtp(email, user.id)
         res.json({id: user.id, isEmailVerified: user.is_email_validated});
     } catch (error: any) {
         console.error(error);
         res.json(error);
     }
 });
-
 
 router.post('/login', async (req: Request, res: Response): Promise<void> => {
     try {
